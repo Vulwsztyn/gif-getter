e3643a2389b1ab1e0699b4126fa69b4f7db3817c
Artur Mostowski <artur.mostowski@currency-one.com>
2020-10-31 12:43:28 +0100

    services

--- pixabayService.js
+++ pixabayService.js
@@ -1,2 +1,23 @@
-class PixabayService {}
+const axios = require('axios')
+const R = require('ramda')
+
+const { Either, left, either } = require('./either')
+
+class PixabayService {
+  async get(q) {
+    const response = () =>
+      axios
+        .get('https://pixabay.com/api/', {
+          params: { ke2y: process.env.PIXABAY_API_KEY, q },
+        })
+        .then((e) => Either.of(e))
+        .catch((_) => left([]))
+    return (await response()).map(
+      R.pipe(
+        R.pathOr([], ['data', 'hits']),
+        R.map((e) => ({ image: e.webformatURL }))
+      )
+    ).$value
+  }
+}
 module.exports = PixabayService
