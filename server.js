0f284c70a78426cb999915305b84d4bf874e1f93
Artur Mostowski <artur.mostowski@currency-one.com>
2020-11-01 11:47:00 +0100

    endpoint

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 class Server {
   constructor(app) {
     this.app = app
-    this.app.get('/', (req, res) => {
+    this.giphyService = giphyService
+    this.pixabayService = pixabayService
+    this.app.get('/api/hello', (req, res) => {
       res.send('Hello World!')
+    })
+    this.app.get('/api/query/:q', async (req, res) => {
+      const { q } = req.params
+      const giphyResponse = await giphyService.get(q)
+      const pixabayResponse = await pixabayService.get(q)
+      console.log(giphyResponse)
+      res.send({ images: [...giphyResponse, ...pixabayResponse] })
     })
   }
   async run(port) {
