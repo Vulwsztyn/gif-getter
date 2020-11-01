2f2040206897ecd123d87032f0780e3a466f2c47
Artur Mostowski <artur.mostowski@currency-one.com>
2020-11-01 13:28:37 +0100

    cors

--- boot.js
+++ boot.js
@@ -3,10 +3,11 @@ require('dotenv').config()
 const Server = require('./server')
 const PixabayService = require('./pixabayService')
 const GiphyService = require('./giphyService')
-const port = 3000
-
+const port = 5000
+const cors = require('cors')
 async function main() {
   const app = express()
+  app.use(cors())
   const giphyService = new GiphyService()
   const pixabayService = new PixabayService()
   const server = new Server(app, giphyService, pixabayService)
